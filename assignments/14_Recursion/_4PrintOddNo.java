51a5d609111f37f0326b68c4e0ef80e3abcf5603
Shivendra Shrivastwa <shivendrasince2001@gmail.com>
2022-12-22 18:26:55 +0530

    committed

---
+++ assignments/14_Recursion/_4PrintOddNo.java
@@ -0,0 +1,17 @@
+public class _4PrintOddNo {
+    public static void printArray(int index, int[] arr){
+        if(index==arr.length){
+            return;
+        }
+
+        if(arr[index]%2!=0){
+            System.out.println(arr[index]);
+        }
+
+        printArray(index+1, arr);
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        printArray(0, arr);
+    }
+}
