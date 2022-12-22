2fced03711a38224b26e75c5687298dc27498e23
Shivendra Shrivastwa <shivendrasince2001@gmail.com>
2022-12-22 18:43:24 +0530

    committed

---
+++ assignments/14_Recursion/_8CharAtEvenPosition.java
@@ -0,0 +1,15 @@
+public class _8CharAtEvenPosition {
+    public static void printCharacter(int ptr, String str){
+        if(ptr==str.length()){
+            return;
+        }
+        if(ptr%2==0){
+            System.out.println(str.charAt(ptr));
+        }
+        printCharacter(ptr+1, str);
+    }
+    public static void main(String[] args) {
+        String str = "Shivendra";
+        printCharacter(0, str);
+    }
+}
