c5aaae16219b86b7fd80d6d828da1b0da8c84702
Shivendra Shrivastwa <shivendrasince2001@gmail.com>
2022-12-23 23:47:35 +0530

    committed

---
+++ assignments/14_Recursion/_14NthFabonacciNo.java
@@ -0,0 +1,10 @@
+public class _14NthFabonacciNo {
+    public static int fabonacci(int n) {
+        if(n==0 || n==1) return n;
+
+        return fabonacci(n-1)+fabonacci(n-2);
+    }
+    public static void main(String[] args) {
+        System.out.println(fabonacci(6));
+    }
+}
