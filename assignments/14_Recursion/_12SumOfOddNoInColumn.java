5120017c91e038d33a4a51678cd8183f7709ff8b
Shivendra Shrivastwa <shivendrasince2001@gmail.com>
2022-12-23 23:38:24 +0530

    committed

---
+++ assignments/14_Recursion/_12SumOfOddNoInColumn.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class _12SumOfOddNoInColumn {
+    public static void printSum(int index, int N, int M, int[][] arr){
+        if (index == M){
+            return;
+        }
+        int sum=0;
+        for(int i=0;i<N;i++){
+            if(arr[i][index]%2!=0){
+                sum+=arr[i][index];
+            }
+        }
+        
+        System.out.println(sum);
+        printSum(index+1,N,M,arr);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ptr = 0;
+        int N= 3;
+        int M = 3;
+        int[][] arr = new int[N][M];
+        System.out.println("Input:-");
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Output:-");
+        printSum(ptr, N, M, arr);
+        sc.close();
+    }
+}
